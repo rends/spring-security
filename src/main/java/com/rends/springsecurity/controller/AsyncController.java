c8345ca3bd3cbb4a3d470483e8932470ec7e3ad0
rends <yourends@163.com>
2018-12-31 17:26:00 +0800

    Initial commit

---
+++ src/main/java/com/rends/springsecurity/controller/AsyncController.java
@@ -0,0 +1,65 @@
+package com.rends.springsecurity.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.UUID;
+import java.util.concurrent.Callable;
+
+@RestController
+public class AsyncController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    @Autowired
+    private MockQueue mockQueue;
+
+    @Autowired
+    private DeferredResultHolder deferredResultHolder;
+
+    @RequestMapping("/orderOne")
+    public Callable<String> orderOne() throws Exception{
+        logger.info("主线程开始");
+
+        Callable<String> result= new Callable<String>(){
+            @Override
+            public String call() throws Exception {
+                logger.info("副线程开始");
+                Thread.sleep(1000);
+                logger.info("副线程结束");
+                return "success";
+            }
+        };
+        return result;
+    }
+
+    @RequestMapping("/orderTwo")
+    public DeferredResult<String> orderTwo() throws Exception {
+        logger.info("主线程开始");
+
+//        String orderNumber = RandomStringUtils.randomNumeric(8);
+        String orderNumber = UUID.randomUUID().toString().replace("-","");
+        mockQueue.setPlaceOrder(orderNumber);
+
+        DeferredResult<String> result = new DeferredResult<>();
+        deferredResultHolder.getMap().put(orderNumber, result);
+
+        return result;
+
+//		Callable<String> result = new Callable<String>() {
+//			@Override
+//			public String call() throws Exception {
+//				logger.info("副线程开始");
+//				Thread.sleep(1000);
+//				logger.info("副线程返回");
+//				return "success";
+//			}
+//		};
+    }
+
+}
